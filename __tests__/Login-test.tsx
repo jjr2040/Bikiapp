87e463eca17c712f75de0ce1413984cc2074a63d
Juan José Villegas <jj.villegas47@gmail.com>
2019-05-04 14:15:50 -0500

    fixed tests

--- __tests__/Login-test.tsx
+++ __tests__/Login-test.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import renderer from 'react-test-renderer'
 import { LoginForm } from '../app/pages/user/LoginForm.page';
 
+jest.useFakeTimers();
 
 test('renders correctly', () => {
   const tree = renderer.create(<LoginForm />).toJSON();
